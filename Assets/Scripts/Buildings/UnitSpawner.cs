86b74f49c722229746dbce0df4132d28095296a5
Matthias Feil <matthiasfeil99@gmx.de>
2023-07-08 12:46:01 +0200

    init

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,43 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform unitSpawnPoint = null;
+
+    #region Server
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject untInstance = Instantiate(unitPrefab, 
+            unitSpawnPoint.position, 
+            unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(untInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
+        if (!isOwned)
+        {
+            return;
+        }
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
